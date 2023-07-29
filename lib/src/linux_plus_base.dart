9ee97078d65a5c9384f8d16020ff0abad52257e0
omegaui <omegaui22@gmail.com>
2023-07-29 14:08:13 +0530

    🔥 version 1.0.1 ready!

--- lib/src/linux_plus_base.dart
+++ lib/src/linux_plus_base.dart
@@ -7,65 +7,66 @@ final class LinuxPlus {
   /// Gets the name of the Linux distribution.
   ///
   /// Returns the name of the Linux distribution as obtained from the '/etc/os-release' file.
-  String get distro => linuxIdentifier.getDistroName();
+  static String get distro => linuxIdentifier.getDistroName();
 
   /// Gets the full user-friendly name of the Linux distribution.
   ///
   /// Returns the full user-friendly name of the Linux distribution as obtained from the '/etc/os-release' file.
-  String get fullDistroName => linuxIdentifier.getFullDistroName();
+  static String get fullDistroName => linuxIdentifier.getFullDistroName();
 
   /// Gets the version of the Linux distribution.
   ///
   /// Returns the version of the Linux distribution as obtained from the '/etc/os-release' file.
-  String get distroVersion => linuxIdentifier.getVersionID();
+  static String get distroVersion => linuxIdentifier.getVersionID();
 
   /// Gets the codename of the Linux distribution version.
   ///
   /// Returns the codename of the Linux distribution version as obtained from the '/etc/os-release' file.
-  String get distroVersionCodename => linuxIdentifier.getVersionCodename();
+  static String get distroVersionCodename =>
+      linuxIdentifier.getVersionCodename();
 
   /// Gets the codename of the Linux distribution.
   ///
   /// Returns the codename of the Linux distribution as obtained from the 'version' field in the '/etc/os-release' file.
-  String get distroCodename => linuxIdentifier.getDistroCodename();
+  static String get distroCodename => linuxIdentifier.getDistroCodename();
 
   /// Gets the name of the parent Linux distribution.
   ///
   /// Returns the name of the parent Linux distribution as obtained from the 'id-like' field in the '/etc/os-release' file.
-  String get parentDistro => linuxIdentifier.getParentDistro();
+  static String get parentDistro => linuxIdentifier.getParentDistro();
 
   /// Checks if the current Linux distribution is Ubuntu.
   ///
   /// Returns true if the Linux distribution is Ubuntu, false otherwise.
-  bool get isUbuntu => linuxIdentifier.isUbuntu();
+  static bool get isUbuntu => linuxIdentifier.isUbuntu();
 
   /// Checks if the current Linux distribution is Debian.
   ///
   /// Returns true if the Linux distribution is Debian, false otherwise.
-  bool get isDebian => linuxIdentifier.isDebian();
+  static bool get isDebian => linuxIdentifier.isDebian();
 
   /// Checks if the current Linux distribution is Fedora.
   ///
   /// Returns true if the Linux distribution is Fedora, false otherwise.
-  bool get isFedora => linuxIdentifier.isFedora();
+  static bool get isFedora => linuxIdentifier.isFedora();
 
   /// Checks if the current Linux distribution is Arch Linux.
   ///
   /// Returns true if the Linux distribution is Arch Linux, false otherwise.
-  bool get isArch => linuxIdentifier.isArch();
+  static bool get isArch => linuxIdentifier.isArch();
 
   /// Checks if the current Linux distribution is either Ubuntu or Debian.
   ///
   /// Returns true if the Linux distribution is either Ubuntu or Debian, false otherwise.
-  bool get isUbuntuOrDebian => linuxIdentifier.isUbuntuOrDebian();
+  static bool get isUbuntuOrDebian => linuxIdentifier.isUbuntuOrDebian();
 
   /// Checks if the current Linux distribution is based on Ubuntu.
   ///
   /// Returns true if the Linux distribution is based on Ubuntu, false otherwise.
-  bool get isUbuntuBased => parentDistro == 'ubuntu';
+  static bool get isUbuntuBased => parentDistro == 'ubuntu';
 
   /// Checks if the current Linux distribution is based on Debian.
   ///
   /// Returns true if the Linux distribution is based on Debian, false otherwise.
-  bool get isDebianBased => parentDistro == 'debian';
+  static bool get isDebianBased => parentDistro == 'debian';
 }
